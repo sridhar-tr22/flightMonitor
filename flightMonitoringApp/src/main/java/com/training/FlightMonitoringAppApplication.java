725bb0d1068e381db46d2a0aa54fc4fac5275891
sridhar-tr22 <62049068+sridhar-tr22@users.noreply.github.com>
2020-03-24 18:44:28 +0530

    project structure created
    dependencies - Spring Boot Web, MySql Connector, Lombok, Spring Jpa.

---
+++ flightMonitoringApp/src/main/java/com/training/FlightMonitoringAppApplication.java
@@ -0,0 +1,13 @@
+package com.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightMonitoringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightMonitoringAppApplication.class, args);
+	}
+
+}
